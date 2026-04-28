46e24b9e2b7dfbd46e5b48f946be334117b7ad57
krejn-0413 <krejn0413@gmail.com>
2026-04-28 23:42:17 +0800

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,5 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/mystic-decider/',     // ← 改成你的仓库名 + 斜杠
+  base: '/mystic-decider/',mystic-decider//
 })
