4617d9747d38647ac5afb2ecfb6554e0e6ee00f4
Griffith . <griffith.@GriffithdeMacBook-Air.local>
2026-04-30 14:28:41 +0800

    修复自定义域名黑屏：移除 BrowserRouter basename='/mystic-decider'

--- src/App.jsx
+++ src/App.jsx
@@ -11,7 +11,7 @@ import QimenPage from './pages/QimenPage';
 
 export default function App() {
   return (
-    <BrowserRouter basename="/mystic-decider">
+    <BrowserRouter>
       <Routes>
         <Route element={<Layout />}>
           <Route path="/" element={<Home />} />
